ee4f599c39b2e70be0a16e70457760b5b1831a18
incuuu <incredible.incuuu92@gmail.com>
2015-07-16 10:27:03 +0300

    Add ‘var’ to Configurator variable

--- js/main.js
+++ js/main.js
@@ -10,43 +10,43 @@
   });
 
   require(['configurator'], function(Configurator) {
-      pictureModuleConfigurator = new Configurator({
-      canvas: document.getElementById('configurator'),
-      backgroundImageUrl: document.getElementById('background').src,
-      scaleViewEl: document.getElementById('scaleRange'),
-      hintEl: document.getElementById('hint'),
-      pixelsPerCentimeter: 6
-    });
+      var pictureModuleConfigurator = new Configurator({
+          canvas: document.getElementById('configurator'),
+          backgroundImageUrl: document.getElementById('background').src,
+          scaleViewEl: document.getElementById('scaleRange'),
+          hintEl: document.getElementById('hint'),
+          pixelsPerCentimeter: 6
+      });
 
-    pictureModuleConfigurator.addPictureModule({
-      left: 100,
-      top: 100
-    });
+      pictureModuleConfigurator.addPictureModule({
+        left: 100,
+        top: 100
+      });
 
-    pictureModuleConfigurator.addPictureModule({
-      left: 320,
-      top: 60,
-      scaleX: 1.4,
-      scaleY: 1.2
-    });
+      pictureModuleConfigurator.addPictureModule({
+        left: 320,
+        top: 60,
+        scaleX: 1.4,
+        scaleY: 1.2
+      });
 
-    pictureModuleConfigurator.addPictureModule({
-      left: 80,
-      top: 330,
-    });
+      pictureModuleConfigurator.addPictureModule({
+        left: 80,
+        top: 330,
+      });
 
-    pictureModuleConfigurator.addPictureModule({
-      left: 340,
-      top: 360,
-      scaleX: 1.2,
-      scaleY: 2
-    });
+      pictureModuleConfigurator.addPictureModule({
+        left: 340,
+        top: 360,
+        scaleX: 1.2,
+        scaleY: 2
+      });
 
-    pictureModuleConfigurator.addPictureModule({
-      left: 400,
-      top: 220,
-      scaleX: 1.7
-    });
+      pictureModuleConfigurator.addPictureModule({
+        left: 400,
+        top: 220,
+        scaleX: 1.7
+      });
   });
 
 })();
